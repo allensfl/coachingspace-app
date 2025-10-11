7f646a96e6abdbb099bb60657eba75191a21c7f6
allensfl <flavien@bluewin.ch>
2025-10-11 11:28:22 +0200

    fix: Behebt die Lesbarkeit des nativen Datumswählers und eine dadurch verursachte Regression

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -7,6 +7,7 @@ const Input = React.forwardRef(({ className, type, ...props }, ref) => {
       type={type}
       className={cn(
         'flex h-10 w-full rounded-md border border-slate-700 bg-slate-900 px-3 py-2 text-sm text-slate-50 ring-offset-slate-950 file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-slate-400 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+        type === 'date' && 'light-datepicker',
         className
       )}
       ref={ref}
